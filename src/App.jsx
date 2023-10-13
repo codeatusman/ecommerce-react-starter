f1018b6f4d5e394dbacc03b660dd43f6a9794f65
usman akbar <vee7475@gmail.com>
2023-10-13 17:30:15 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='app'>
+      app
+    </main>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+export const jacketsData = [
+    {
+        thumbnail: "/thumbnailjacket1.png",
+        jacket: "/jacket1.png",
+    },
+    {
+        thumbnail: "/thumbnailjacket2.png",
+        jacket: "/jacket2.png",
+    },
+    {
+        thumbnail: "/thumbnailjacket3.png",
+        jacket: "/jacket3.png",
+    },
+];
+
+export const categoryData = [
+    {
+        imgURL: '/ctg1.png',
+        title: "shoes",
+    },
+    {
+        imgURL: '/ctg2.png',
+        title: "dress",
+    },
+    {
+        imgURL: '/ctg3.png',
+        title: "cosmetics",
+    },
+    {
+        imgURL: '/ctg4.png',
+        title: "watches",
+    },
+];
+
+export const blogData = [
+    {
+        imgURL: "/blog1.jpg",
+        ctg: 'Fashion', 
+        title: 'Top 10 Deals', 
+        info: 'By Mr Admin - 07 Sep, 2023'
+    },
+    {
+        imgURL: "/blog2.jpg",
+        ctg: 'Trends',         
+        title: 'How to look beautiful', 
+        info: 'By Mr Admin - 17 Sep, 2023'
+    },
+    {
+        imgURL: "/blog3.jpg",
+        ctg: 'Deals',         
+        title: 'Hot Summer Collection', 
+        info: 'By Mr Admin - 27 Sep, 2023'
+    },
+    {
+        imgURL: "/blog4.jpg",
+        ctg: 'Collection',         
+        title: 'Ideas to improve life', 
+        info: 'By Mr Admin - 29 Sep, 2023'
+    }
+]
+ 
+export const swiperSettings = {
+    slidesPerView: 1, 
+    spaceBetween: 30,
+    breakpoints: {
+        480: {
+            slidesPerView: 1
+        },
+        600: {
+            slidesPerView: 2
+        },
+        800: {
+            slidesPerView: 3
+        },
+        1100: {
+            slidesPerView: 4
+        },
+        1300: {
+            slidesPerView: 5
+        }
+    }
+
+}
